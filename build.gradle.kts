7a821aae7c0c32db5158e46cfde948ad56903f62
leehoyong <lee199402@gmail.com>
2021-11-25 21:33:25 +0900

    setup spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,31 +7,11 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:5.15.0")
     }
 }
 
 subprojects {
-    val ktlint: Configuration by configurations.creating
-    dependencies {
-        ktlint("com.pinterest:ktlint:0.42.1")
-    }
-
-    tasks.register<JavaExec>("ktlint") {
-        group = "verification"
-        description = "Check Kotlin code style."
-        classpath = ktlint
-        main = "com.pinterest.ktlint.Main"
-        args("src/**/*.kt")
-    }
-
-    tasks.register<JavaExec>("ktlintFormat") {
-        group = "formatting"
-        description = "Fix Kotlin code style deviations."
-        classpath = ktlint
-        main = "com.pinterest.ktlint.Main"
-        args("-F", "src/**/*.kt")
-    }
-
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
             jvmTarget = "11"
@@ -53,6 +33,11 @@ subprojects {
         }
     }
 
+    afterEvaluate {
+        project.apply("$rootDir/spotless.gradle")
+        // used ./gradlew spotlessCheck
+        // used ./gradlew spotlessApply
+    }
 }
 
 pluginManager.withPlugin("kotlin-kapt") {
