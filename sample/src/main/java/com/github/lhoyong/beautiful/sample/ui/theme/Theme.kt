529c9a96bcea0fd23afc8a2b82b3a6054cd84521
leehoyong <lee199402@gmail.com>
2021-11-25 21:33:35 +0900

    Fixed lint

--- sample/src/main/java/com/github/lhoyong/beautiful/sample/ui/theme/Theme.kt
+++ sample/src/main/java/com/github/lhoyong/beautiful/sample/ui/theme/Theme.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed by 2021 lhoyong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.lhoyong.beautiful.sample.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -7,15 +22,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
 
         /* Other default colors to override
     background = Color.White,
@@ -36,9 +51,9 @@ fun BeautifulTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composa
     }
 
     MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
     )
 }
