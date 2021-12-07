751a658623f380cd22a2c0250a87802529e4c95b
lhoyong <lee199402@gmail.comc>
2021-12-08 01:12:22 +0900

    Changed internalItems from remember to rememberSaveable

--- beautiful-swipe/src/main/java/com/github/lhoyong/beautiful/swipe/BeautifulSwipe.kt
+++ beautiful-swipe/src/main/java/com/github/lhoyong/beautiful/swipe/BeautifulSwipe.kt
@@ -26,7 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,7 +43,7 @@ fun <T> BeautifulSwipe(
     items: List<T>,
     content: @Composable (T) -> Unit
 ) {
-    var itemsInternal by remember { mutableStateOf(items) }
+    var itemsInternal by rememberSaveable { mutableStateOf(items) }
     val rememberSwipeState = rememberSwipeState()
 
     rememberSwipeState.onAnimationEnd = {
