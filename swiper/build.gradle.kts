c5733c034858ed93abf67c043ec2d5b537196459
lhoyong <lee199402@gmail.comc>
2021-12-17 20:40:10 +0900

    Clear unused dependencies

--- swiper/build.gradle.kts
+++ swiper/build.gradle.kts
@@ -53,29 +53,18 @@ dependencies {
     implementation(Dep.Kotlin.stdlib)
     implementation(Dep.Kotlin.coroutine)
 
-    implementation(Dep.Android.core)
-    implementation(Dep.Android.appcompat)
-
-    implementation(Dep.Compose.constraintLayout)
-
     implementation(Dep.Compose.ui)
     implementation(Dep.Compose.uiTooling)
 
     implementation(Dep.Compose.foundation)
 
     implementation(Dep.Compose.material)
-    implementation(Dep.Compose.materialIcon)
-    implementation(Dep.Compose.materialIconsExtended)
 
-    implementation(Dep.Compose.activity)
     implementation(Dep.Compose.runtime)
-
-    implementation(Dep.Compose.accompanist)
 
     testImplementation(Dep.Test.junit)
 
     androidTestImplementation(Dep.Compose.activity)
-
     androidTestImplementation(Dep.AndroidTest.junitExt)
     androidTestImplementation(Dep.AndroidTest.espresso)
     androidTestImplementation(Dep.AndroidTest.compose)
